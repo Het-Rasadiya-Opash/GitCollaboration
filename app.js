e10ecc337eda5ea0f22101a27cbc9f41266b7e11
Het <hra.opash@gmail.com>
2026-04-07 16:10:56 +0530

    api

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ const app = express();
 
 app.use(express.json());
 
+app.use("/api/v1", (req, res) => {
+  res.json({
+    success: true,
+    message: "Welcome to the API",
+  });
+});
+
 app.listen(3000, () => {
   console.log;
 });
